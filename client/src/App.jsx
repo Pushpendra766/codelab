9f2b39951e7c5d294cb2936a5add6bcde1f49de4
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-05-09 20:18:36 +0530

    basic functionaity added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,89 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import "./components/init.js";
 import "./App.css";
 import Editor from "./components/Editor";
 
 function App() {
+  const sampleHtml = `<h1>I am a headline made with HTML</h1>
+  <p>And I am a simple text paragraph. The color of this text is styled with CSS. Click the button below to remove me through the power JavaScript.</p>
+  <button>Hide the text above</button>`;
+  const sampleCss = `body {
+    font-family: sans-serif;
+    text-align: center;
+    padding: 3rem;
+    font-size: 1.125rem;
+    line-height: 1.5;
+    transition: all 725ms ease-in-out;
+  }
+  
+  h1 {
+    font-size: 2rem;
+    font-weight: bolder;
+    margin-bottom: 1rem;
+  }
+  
+  p {
+    margin-bottom: 1rem;
+    color: tomato;
+  }
+  
+  button {
+    cursor: pointer;
+    appearance: none;
+    border-radius: 4px;
+    font-size: 1.25rem;
+    padding: 0.75rem 1rem;
+    border: 1px solid navy;
+    background-color: dodgerblue;
+    color: white;
+  }`;
+  const sampleJavascript = `$('button').on('click', function() {
+    $('p').css('opacity', 0);
+  });`;
+  const [html, setHtml] = useState(sampleHtml);
+  const [css, setCss] = useState(sampleCss);
+  const [javascript, setJavascript] = useState(sampleJavascript);
+  const [srcDoc, setSrcDoc] = useState();
+  useEffect(() => {
+    setTimeout(() => {
+      setSrcDoc(`<html>
+      <body>${html}</body>
+      <style>${css}</style>
+      <script>${javascript}</script>
+    </html>`);
+    }, 250);
+  }, [html, css, javascript]);
   return (
     <div className="mx-0">
-      <nav className="bg-[#fff000]">Navbar</nav>
-      <div className="bg-[#ffff20] w-full flex flex-row justify-around">
-        <div>html</div>
-        <div>css</div>
-        <div>js</div>
+      {/* <nav className="bg-[#fff000]">Navbar</nav> */}
+      <div className="w-full flex flex-row justify-around text-base gap-2">
+        <Editor
+          displayName="HTML"
+          language="html"
+          code={html}
+          handleChange={setHtml}
+        />
+        <Editor
+          displayName="CSS"
+          language="css"
+          code={css}
+          handleChange={setCss}
+        />
+        <Editor
+          displayName="JavaScript"
+          language="js"
+          code={javascript}
+          handleChange={setJavascript}
+        />
       </div>
-      <div>Output</div>
+      <div>
+        <iframe
+          className="w-full h-80"
+          srcDoc={srcDoc}
+          title="output"
+          sandbox="allow-scripts"
+        />
+      </div>
     </div>
   );
 }
--- client/src/components/Editor.jsx
+++ client/src/components/Editor.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import CodeMirror from "@uiw/react-codemirror";
+import { EditorView } from '@codemirror/view';
+import { javascript } from "@codemirror/lang-javascript";
+import { githubDark } from "@uiw/codemirror-theme-github";
+import { html } from "@codemirror/lang-html";
+import { css } from "@codemirror/lang-css";
+import { ImHtmlFive } from "react-icons/im";
+import { DiCss3 } from "react-icons/di";
+import { IoLogoJavascript } from "react-icons/io";
 
-const Editor = () => {
+function Editor(props) {
+  const { displayName, language, code, handleChange } = props;
+  const onChange = React.useCallback((value, viewUpdate) => {
+    console.log("value:", value);
+    handleChange(value);
+  }, []);
+  const lang =
+    (language === "html" && html({ html: true })) ||
+    (language === "css" && css({ css: true })) ||
+    (language === "js" && javascript({ jsx: true }));
   return (
-    <div className='text-3xl font-bold underline'>Editor</div>
-  )
+    <div className="w-1/3">
+      <div className="flex justify-center gap-4">
+        {language === "html" && <ImHtmlFive size={20} />}
+        {language === "css" && <DiCss3 size={20} />}
+        {language === "js" && <IoLogoJavascript size={20} />}
+        {displayName}
+      </div>
+      <CodeMirror
+        value={code}
+        height="350px"
+        theme={githubDark}
+        extensions={[lang, EditorView.lineWrapping]}
+        onChange={onChange}
+      />
+    </div>
+  );
 }
-
-export default Editor
+export default Editor;
