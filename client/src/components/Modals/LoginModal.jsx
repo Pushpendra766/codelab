d9044d90610eb0317e32af5e3c0be855a595cbf7
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-05-14 01:00:40 +0530

    feat: login/signup functionality added

--- client/src/components/Modals/LoginModal.jsx
+++ client/src/components/Modals/LoginModal.jsx
@@ -1,27 +1,147 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Modal from "./Modal";
 import { GrClose } from "react-icons/gr";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  setPersistence,
+  browserSessionPersistence,
+} from "firebase/auth";
+import { auth } from "../../firebase";
+import toast, { Toaster } from "react-hot-toast";
 
-const LoginModal = ({ isModalOpen, setIsModalOpen }) => {
+const LoginModal = ({
+  isModalOpen,
+  setIsModalOpen,
+  isSignup,
+  setIsSignup,
+  setIsLoggedIn,
+}) => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [confirmPassword, setConfirmPassword] = useState("");
   const handleCloseModal = () => {
     setIsModalOpen(false);
   };
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    signInWithEmailAndPassword(auth, email, password)
+      .then((userCredential) => {
+        setIsModalOpen(false);
+        toast.success("Logged in Successfully!");
+        setIsLoggedIn(true);
+      })
+      .catch((error) => {
+        console.log(error);
+        toast.error("Invalid email or password!");
+      });
+    setPersistence(auth, browserSessionPersistence)
+      .then(() => {
+        console.log("Persistent");
+        
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  const handleSignup = (e) => {
+    e.preventDefault();
+    if (password !== confirmPassword) {
+      toast.error("Confirm password doesn't match!", {
+        duration: 2000,
+      });
+    } else {
+      createUserWithEmailAndPassword(auth, email, password)
+        .then((userCredential) => {
+          setIsModalOpen(false);
+          toast.success("Account Created Successfully!");
+          setIsLoggedIn(true);
+        })
+        .catch((error) => {
+          if (error.code === "auth/email-already-in-use") {
+            toast.error("Email already exists!");
+          } else {
+            toast.error("Something went wrong!");
+          }
+        });
+    }
+  };
+
   return (
     <div className="container mx-auto">
+      <Toaster />
       <Modal isModalOpen={isModalOpen} onClose={handleCloseModal}>
         <div className="bg-white p-6">
           <div className="flex justify-between">
-            <h2 className="text-xl font-bold mb-4">Login</h2>
+            <h2 className="text-xl font-bold mb-4">
+              {isSignup ? "Sign Up" : "Login"}
+            </h2>
             <button onClick={handleCloseModal}>
               <GrClose />
             </button>
           </div>
           <div className="flex flex-col gap-4">
-           <p>Username : <input type="text" className="border rounded-md"/></p>
-           <p>Password : <input type="text" className="border rounded-md"/></p>
-           <button className="text-[#ffffff] bg-[#2C74B3] px-4 py-1 rounded-full font-semibold hover:bg-[#205295]">Login</button>
-           <hr/>
-           <button className="text-[#ffffff] bg-[#16a34a] px-4 py-1 rounded-full font-semibold hover:bg-[#55ad33] sm:w-1/2 mx-auto whitespace-nowrap">Create new account </button>
+            {isSignup ? (
+              <form className="flex flex-col gap-4" onSubmit={handleSignup}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="py-2 pl-2 border-2 rounded-md"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="py-2 pl-2 border-2 rounded-md"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Confirm Password"
+                  className="py-2 pl-2 border-2 rounded-md"
+                  onChange={(e) => setConfirmPassword(e.target.value)}
+                />
+                <button
+                  className="text-[#ffffff] bg-[#2C74B3] px-4 py-2 rounded-full font-semibold hover:bg-[#205295]"
+                  type="submit"
+                >
+                  Sign Up
+                </button>
+              </form>
+            ) : (
+              <form className="flex flex-col gap-4" onSubmit={handleLogin}>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="py-2 pl-2 border-2 rounded-md"
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="password"
+                  placeholder="Password"
+                  className="py-2 pl-2 border-2 rounded-md"
+                  onChange={(e) => setPassword(e.target.value)}
+                />
+                <button
+                  className="text-[#ffffff] bg-[#2C74B3] px-4 py-2 rounded-full font-semibold hover:bg-[#205295]"
+                  type="submit"
+                >
+                  Login
+                </button>
+              </form>
+            )}
+
+            <hr />
+            <button
+              className="text-[#ffffff] bg-[#16a34a] px-4 py-2 rounded-full font-semibold hover:bg-[#55ad33] sm:w-1/2 mx-auto whitespace-nowrap"
+              onClick={() => {
+                setIsSignup(!isSignup);
+              }}
+            >
+              {isSignup ? "Already have an account" : "Create new account"}
+            </button>
           </div>
         </div>
       </Modal>
--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,19 +1,72 @@
-import React from "react";
+import React, { useState } from "react";
 import { SiApostrophe } from "react-icons/si";
+import { BiUserCircle } from "react-icons/bi";
+import { CgLogOut } from "react-icons/cg";
+import { signOut } from "firebase/auth";
+import { auth } from "../firebase";
+import toast, { Toaster } from "react-hot-toast";
 
-const Navbar = ({setIsModalOpen}) => {
-  const handleOpen = ()=>{
+const Navbar = ({ setIsModalOpen, setIsSignup, isLoggedIn, setIsLoggedIn }) => {
+  const [openDropdown, setOpenDropdown] = useState(false);
+  const handleOpen = (mode) => {
+    if (mode === "signup") {
+      setIsSignup(true);
+    } else {
+      setIsSignup(false);
+    }
     setIsModalOpen(true);
-  }
+  };
+  const handleLogout = () => {
+    signOut(auth)
+      .then(() => {
+        toast.success("Logged out!");
+        setIsLoggedIn(false);
+        setOpenDropdown(false);
+      })
+      .catch((error) => {
+        toast.error("Something went wrong!");
+      });
+  };
+
   return (
-    <div className="bg-[#0A2647] py-2 sm:py-3 px-4 sm:px-10 flex justify-between">
-      <div className="text-[#ffffff] text-xl font-semibold flex gap-2 sm:gap-4">
-        <SiApostrophe size={25}/> Codelab
+    <div>
+      <Toaster />
+      <div className="bg-[#0A2647] py-2 sm:py-3 px-4 sm:px-10 flex justify-between">
+        <div className="text-[#ffffff] text-xl font-semibold flex gap-2 sm:gap-4">
+          <SiApostrophe size={25} /> Codelab
+        </div>
+        {isLoggedIn ? (
+          <button
+            className="text-[#ffffff] text-lg font-semibold"
+            onClick={() => setOpenDropdown(!openDropdown)}
+          >
+            <BiUserCircle size={30} />
+          </button>
+        ) : (
+          <div className="flex gap-2 sm:gap-4">
+            <button
+              className="text-[#ffffff] bg-[#2C74B3] px-3 sm:px-4 text-sm sm:text-base py-1 rounded-full font-semibold hover:bg-[#205295]"
+              onClick={() => handleOpen("login")}
+            >
+              Login
+            </button>
+            <button
+              className="text-[#ffffff] bg-[#2C74B3] px-3 sm:px-4 text-sm sm:text-base py-1 rounded-full font-semibold hover:bg-[#205295]"
+              onClick={() => handleOpen("signup")}
+            >
+              Signup
+            </button>
+          </div>
+        )}
       </div>
-      <div className="flex gap-2 sm:gap-4">
-        <button className="text-[#ffffff] bg-[#2C74B3] px-3 sm:px-4 text-sm sm:text-base py-1 rounded-full font-semibold hover:bg-[#205295]" onClick={handleOpen}>Login</button>
-        <button className="text-[#ffffff] bg-[#2C74B3] px-3 sm:px-4 text-sm sm:text-base py-1 rounded-full font-semibold hover:bg-[#205295]" onClick={handleOpen}>Signup</button>
-      </div>
+      {openDropdown && (
+        <span className="absolute right-2 sm:right-10 bg-[#ffffff] py-2 px-4 rounded-md ">
+          <button className="flex gap-2" onClick={handleLogout}>
+            <CgLogOut size={20} className="mt-1" />
+            Logout{" "}
+          </button>
+        </span>
+      )}
     </div>
   );
 };
