9f56513b841e25d79027626b586c9cc4c43e6d2b
Pushpendra Chandravanshi <pushpendrachandravanshi4@gmail.com>
2023-05-15 18:37:31 +0530

    refactor: minor changes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -27,7 +27,6 @@ function App() {
   const [prompt, setPrompt] = useState("");
 
   const fetchFiles = () => {
-    console.log(currentUserId);
     const usersRef = collection(db, "users");
     const userDocRef = doc(usersRef, currentUserId);
 
@@ -35,7 +34,6 @@ function App() {
       .then((doc) => {
         const f = doc.data().file;
         setFiles(f);
-        console.log("Files for user", currentUserId, ":", f);
       })
       .catch((error) => {
         console.error("Error retrieving files for user:", error);
